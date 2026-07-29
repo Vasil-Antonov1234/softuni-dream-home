f87b83a54a6b4894d1813cc1abc0429dc4d85381
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-07-29 17:07:35 +0300

    Add createPropertiesSchema

--- src/controllers/propertyController.js
+++ src/controllers/propertyController.js
@@ -2,8 +2,16 @@ import { Router } from "express";
 
 const propertyController = Router();
 
-propertyController.get("/create", async (req, res) => {
+propertyController.get("/create", (req, res) => {
     res.render("properties/create");
+})
+
+propertyController.post("/create", async (req, res) => {
+    const data = req.body;
+
+    console.log(data)
+
+    res.redirect("/");
 })
 
 export default propertyController;
---
+++ src/schemas/propertiesSchema.js
@@ -0,0 +1,21 @@
+import * as z from "zod";
+
+export const createPropertiesSchema = z.object({
+    type: z.string()
+        .min(4, { error: "Type should be at least 4 characters" }),
+    location: z.string()
+        .min(3, { error: "Location should be at least 3 characters" }),
+    area: z.coerce.number()
+        .min(2, { error: "Area should be at least 2 digits" }),
+    price: z.coerce.number()
+        .min(1, { error: "Price should be at least 1" })
+        .max(10, { error: "Price should be at most 10 digits long" })
+        .nonnegative({ error: "Price cannot be negative number" }),
+    image: z.httpUrl({ error: "Invalid URL address" }),
+    contact: z.string()
+        .length(10, { error: "Contact should be characters long" })
+        .regex(/^\d+$/, { error: "Contact must contain only digits" })
+        .transform((x) => Number(x)),
+    description: z.string()
+        .min(10, { error: "Descriptiom must be at least 10 characters long"})
+})
